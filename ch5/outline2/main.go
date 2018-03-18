c92f5bbcf558dac7d26d0c5379f54e9cb4487f7d
Leonty Chudinov <leonty@inbox.ru>
2018-03-18 07:41:43 +0500

    more work done

--- ch5/outline2/main.go
+++ ch5/outline2/main.go
@@ -8,19 +8,21 @@ import (
 	"golang.org/x/net/html"
 )
 
-var depth int
-
-func startElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
-		depth++
+func outline(doc *html.Node) {
+	var depth int
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
 	}
-}
-func endElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		depth--
-		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
 	}
+	forEachNode(doc, startElement, endElement)
 }
 
 func main() {
@@ -39,7 +41,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
 		os.Exit(1)
 	}
-	forEachNode(doc, startElement, endElement)
+	outline(doc)
 }
 
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
