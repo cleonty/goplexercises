ac1db0a473977221d26835c89876d395c5a3c30c
Leonty Chudinov <leonty@inbox.ru>
2018-05-21 11:37:36 +0500

    reverb

---
+++ ch8/netcat2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	var address = "localhost:8080"
+	if len(os.Args) == 3 {
+		address = os.Args[1] + ":" + os.Args[2]
+	}
+	log.Printf("Connecting to %s\n", address)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	go mustCopy(os.Stdout, conn)
+	mustCopy(conn, os.Stdin)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+
+	}
+}
---
+++ ch8/reverb2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	var address = "localhost:8080"
+	if len(os.Args) > 1 {
+		address = os.Args[1]
+	}
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Printf("client connected")
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToTitle(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
