addc84373216573202666c21f2c15f06545eb2f0
Leonty Chudinov <leonty@inbox.ru>
2018-08-22 14:47:04 +0500

    mirroed query
    Signed-off-by: Leonty Chudinov <leonty@inbox.ru>

--- ch8/fetch8.11/main.go
+++ ch8/fetch8.11/main.go
@@ -2,35 +2,86 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"io"
 	"net/http"
-	"os"
+	"sync"
 )
 
-func fetch(url string) (body string) {
+type Response struct {
+	body string
+	err  error
+}
+
+func fetch(ctx context.Context, url string, responses chan<- Response) {
 	var b bytes.Buffer
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return ""
+		fmt.Printf("fetch: NewRequest: %v\n", err)
+		responses <- Response{"", err}
+		return
 	}
+	request = request.WithContext(ctx)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return ""
+		fmt.Printf("fetch: do: %v\n", err)
+		responses <- Response{"", err}
+		return
 	}
 	defer resp.Body.Close()
 	_, err = io.Copy(&b, resp.Body)
-	return b.String()
+	responses <- Response{b.String(), nil}
 }
 
 func mirroredQuery() string {
-	responses := make(chan string, 3)
-	go func() { responses <- fetch("asia.gopl.io") }()
-	go func() { responses <- fetch("europe.gopl.io") }()
-	go func() { responses <- fetch("americas.gopl.io") }()
+	responses := make(chan Response, 3)
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer fmt.Println("end 1")
+		fetch(ctx, "http://google.ru", responses)
+	}()
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer fmt.Println("end 2")
+		fetch(ctx, "http://google.ru", responses)
+	}()
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer fmt.Println("end 3")
+		fetch(ctx, "http://google.ru", responses)
+	}()
+	fmt.Println("waiting for response")
+	errCount := 0
+	var response Response
+	for {
+		response = <-responses
+		if response.err != nil {
+			fmt.Printf("got error: %v\n", response.err)
+			errCount++
+			if errCount > 2 {
+				return ""
+			}
+			continue
+		}
+		fmt.Println("got response!")
+		break
+	}
+	fmt.Println("cancel other go rounites!")
+	cancel()
+	fmt.Println("waiting for goroutines")
+	wg.Wait()
+	fmt.Println("waiting for goroutines done")
+	return response.body
 }
 
 func main() {
-	for _, url := range os.Args[1:] {
-
-	}
+	response := mirroredQuery()
+	fmt.Println(response)
+	panic("check")
 }
