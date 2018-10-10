10b1712ab61f055887097af282300f4517ab8b6f
Leonty Chudinov <leonty@inbox.ru>
2018-10-10 08:58:37 +0500

    search

---
+++ ch12/search/search.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/cleonty/gopl/ch12/params"
+)
+
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(resp, "Search: %+v\n", data)
+}
+
+func main() {
+	http.HandleFunc("/", search)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
